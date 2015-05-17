7b19dd809d6136c1da231331a24bb773c57204f2
gary-b <garybarnett101@gmail.com>
2015-05-17 19:39:14 +0100

    Bug: creation of arguments after wf root deleted.

--- js/directives/wfDesigner.js
+++ js/directives/wfDesigner.js
@@ -16,7 +16,7 @@ app.directive('wfDesigner', function(wfGraphManipulator, designerUI) {
                 }
                 if (designerUI.selectedItem === scope.wfDesigner) { //root of workflow
                     scope.wfDesigner = null;
-                    scope.arguments = null;
+                    scope.arguments = [];
                 } else {
                     wfGraphManipulator.deleteWfPart(designerUI.selectedItem);
                 }
